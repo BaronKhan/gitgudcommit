6197279e774178b814b79f187cad4d7744a9ac29
BaronKhan <bksonic@hotmail.co.uk>
2018-12-27 17:22:17 +0000

    git_http_proxy.js: add circuit breaker

--- web/src/git_http_proxy.js
+++ web/src/git_http_proxy.js
@@ -7,60 +7,72 @@ const https = require('https');
 const fs = require('fs');
 
 const mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css",
-    "wasm": "application/wasm"
+  "html": "text/html",
+  "js": "text/javascript",
+  "css": "text/css",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "gif": "image/gif",
+  "wasm": "application/wasm"
 };
+
+// Maximum 100 requests every minute; else activate circuit breaker
+const requestLimit = 100*14; //12 for loading page + 2 for GitHub proxy
+const requestBreakTime = 60*1000;
 
 var requestCount = 0;
 
 function onRequest(request, response) {
-    let path = request.url.substring(1);
+  let path = request.url.substring(1);
 
-    if(path.indexOf('/') > -1) {  
-        const options = {
-            hostname: 'github.com',
-            port: 443,
-            path: request.url,
-            method: request.method
-        };
+  if (requestCount > requestLimit) {
+    console.log("Circuit breaker is active. Ignoring request for "+path);
+  } else {
 
-        console.log(`Proxying ${options.method} request to ${options.hostname} with path ${options.path}`);
-        const proxy = https.request(options, function (res) {
-            res.pipe(response, {
-            end: true
-            });
+    if(path.indexOf('/') > -1) {
+      const options = {
+        hostname: 'github.com',
+        port: 443,
+        path: request.url,
+        method: request.method
+      };
+
+      console.log(`Proxying ${options.method} request to ${options.hostname} with path ${options.path}`);
+      const proxy = https.request(options, function (res) {
+        res.pipe(response, {
+        end: true
         });
+      });
 
-        request.pipe(proxy, {
-            end: true
-        });
+      request.pipe(proxy, {
+        end: true
+      });
 
-        requestCount++;
     } else {
-        if(path === '') {
-            path = 'index.html';
-        }
+      if(path === '') {
+        path = 'index.html';
+      }
 
-        var mimeType = mimeTypes[path.split('.').pop()];
+      var mimeType = mimeTypes[path.split('.').pop()];
       
-        if (!mimeType) {
-            mimeType = 'text/plain';
-        }
+      if (!mimeType) {
+        mimeType = 'text/plain';
+      }
 
-        response.writeHead(200, { "Content-Type": mimeType });
-        
-        if(fs.existsSync(path)) {
-            response.end(fs.readFileSync(path));
-        } else {
-            response.statusCode = 404;
-            response.end('');
-        } 
+      response.writeHead(200, { "Content-Type": mimeType });
+
+      if(fs.existsSync(path)) {
+        response.end(fs.readFileSync(path));
+      } else {
+        response.statusCode = 404;
+        response.end('');
+      }
     }
+
+    requestCount++;
+    setTimeout(function() {requestCount=0; }, requestBreakTime);
+  }
 }
 
 http.createServer(onRequest).listen(5000);
--- web/src/main.js
+++ web/src/main.js
@@ -120,6 +120,7 @@ function analyseCommits() {
     analysisProgress = 50 + Math.ceil(((i+1)*50.0)/numCommits);
     setProgressBar();
   }
+  console.log("Finished analysing commits");
   analysisInProgress = false;
 }
 
@@ -191,7 +192,7 @@ function createSuccessAlert(text) {
   if(document.getElementById("analyseSuccess") == null) {
     var successAlert = document.createElement("div");
     successAlert.id = "analyseSuccess";
-    successAlert.className="alert alert-success alert-dismissible fade in";
+    successAlert.className="alert alert-success alert-dismissible fade in text-center";
     successAlert.innerHTML="<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\
                             <strong>Success!</strong> "+text;
     var panelRepo = document.getElementById("panelRepo");
@@ -203,7 +204,7 @@ function createFailureAlert(text) {
   if(document.getElementById("analyseFailure") == null) {
     var failureAlert = document.createElement("div");
     failureAlert.id = "analyseFailure";
-    failureAlert.className="alert alert-danger alert-dismissible fade in";
+    failureAlert.className="alert alert-danger alert-dismissible fade in text-center";
     failureAlert.innerHTML="<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\
                             <strong>Error:</strong> "+text;
     var panelRepo = document.getElementById("panelRepo");
@@ -220,64 +221,71 @@ document.getElementById("uploadZip").onchange = function() {
   gitworker.postMessage({'cmd': 'resetcount'})
   var zip = new JSZip();
   fileCount = 0;
-  zip.loadAsync(this.files[0]).then(function(contents) {
-    // process ZIP file content here
-    try {
-      var isRepo = false;
-      Object.keys(contents.files).forEach(function(filename) {
-        var pattern = ".git/";
-        if (filename.includes(pattern)) {
-          if (!isRepo) {
-            repoName = filename.substr(0, filename.indexOf(pattern));
-            var urlInput = document.getElementById("urlInput");
-            urlInput.value = repoName.substring(0, repoName.length - 1);
-            isRepo = true;
-          }
-        }
 
-        if (!filename.endsWith("/")) {
-          fileCount++;
+  try {
+
+    if(this.files[0].size > 1073741824){
+       throw new Error("File is too big! The maximum file size is 1 GB.");
+    };
+
+    zip.loadAsync(this.files[0]).then(function(contents) {
+      // process ZIP file content here
+        var isRepo = false;
+        Object.keys(contents.files).forEach(function(filename) {
+          var pattern = ".git/";
+          if (filename.includes(pattern)) {
+            if (!isRepo) {
+              repoName = filename.substr(0, filename.indexOf(pattern));
+              var urlInput = document.getElementById("urlInput");
+              urlInput.value = repoName.substring(0, repoName.length - 1);
+              isRepo = true;
+            }
+          }
+
+          if (!filename.endsWith("/")) {
+            fileCount++;
+          }
+        });
+        if (!isRepo) {
+          throw new Error("The ZIP file does not contain a Git repository.");
         }
-      });
-      if (!isRepo) {
-        throw new Error("The ZIP file does not contain a Git repository.");
-      }
-      else {
-        console.log("Extracting "+fileCount+" files")
-        //Create all the folders first, then the files
-        console.log("Creating folders")
-        Object.keys(contents.files).forEach(function(filename) {
-          if (/*filename.includes(".git/") && */filename.endsWith("/")) {
-            zip.files[filename].async('uint8array').then(function (fileData) {
-              // console.log(filename)
-              gitworker.postMessage({
-                'cmd': 'createfolder',
-                'folder_name': filename});
-            });
-          }
-        });
-        console.log("Creating files")
-        Object.keys(contents.files).forEach(function(filename) {
-          if (/*filename.includes(".git/") && */!filename.endsWith("/")) {
-            zip.files[filename].async('uint8array').then(function (fileData) {
-              // console.log(filename)
-              gitworker.postMessage({
-                'cmd': 'createfile',
-                'file_name': filename,
-                'file_data': fileData});
-            });
-          }
-        });
-      }
-    }
-    catch (e) {
-      createFailureAlert(e.message);
+        else {
+          console.log("Extracting "+fileCount+" files")
+          //Create all the folders first, then the files
+          console.log("Creating folders")
+          Object.keys(contents.files).forEach(function(filename) {
+            if (/*filename.includes(".git/") && */filename.endsWith("/")) {
+              zip.files[filename].async('uint8array').then(function (fileData) {
+                // console.log(filename)
+                gitworker.postMessage({
+                  'cmd': 'createfolder',
+                  'folder_name': filename});
+              });
+            }
+          });
+          console.log("Creating files")
+          Object.keys(contents.files).forEach(function(filename) {
+            if (/*filename.includes(".git/") && */!filename.endsWith("/")) {
+              zip.files[filename].async('uint8array').then(function (fileData) {
+                // console.log(filename)
+                gitworker.postMessage({
+                  'cmd': 'createfile',
+                  'file_name': filename,
+                  'file_data': fileData});
+              });
+            }
+          });
+        }
+    }, function() {
+      createFailureAlert("ZIP file does not contain a Git Repository.");
+      this.value = "";
       analysisInProgress = false;
       hideProgressBar();
-    }
-  }, function() {
-    createFailureAlert("ZIP file does not contain a Git Repository.");
+    });
+  } catch (e) {
+    createFailureAlert(e.message);
+    this.value = "";
     analysisInProgress = false;
     hideProgressBar();
-  }); 
+  }
 }
