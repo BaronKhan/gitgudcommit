395619abc1ca9175501784884a311ebf44a15645
BaronKhan <bksonic@hotmail.co.uk>
2018-12-31 13:17:19 +0000

    Update jsgitgudcore.cpp

--- web/jsgitgudcore.cpp
+++ web/jsgitgudcore.cpp
@@ -1,10 +1,12 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <emscripten/bind.h>
-
-using namespace emscripten;
+#include <string>
 
 #include "tagger.hpp"
+#include "gitgudcommit.hpp"
+
+using namespace emscripten;
 
 void jsInitPosTagger()
 {
@@ -15,7 +17,20 @@ void jsInitPosTagger()
 }
 
 EMSCRIPTEN_BINDINGS(my_module) {
-    function("jsInitPosTagger", &jsInitPosTagger);
+  function("jsInitPosTagger", &jsInitPosTagger);
+  register_vector<std::string>("vector<string>");
+  register_vector<std::pair<unsigned, std::string>>("vector<pair<unsigned, string>>");
+
+  class_<GitGud::Commit>("Commit")
+    .constructor< const std::string &, const std::string &,
+                  const std::string &, long>()
+    .property("author", &GitGud::Commit::getAuthor)
+    .property("email", &GitGud::Commit::getEmail)
+    .property("message", &GitGud::Commit::getMessage)
+    .property("timestamp", &GitGud::Commit::getTimestamp)
+    .function("getCommitScore", &GitGud::Commit::getCommitScore)
+    .function("getSuggestions", &GitGud::Commit::getSuggestions)
+    ;
 }
 
 #endif
